8947f0932687d313984d4a3600d8a2b26dd12486
unknown <elisabethe91@gmail.com>
2018-10-03 21:31:50 +0200

    add function onClick

---
+++ calc.js
@@ -0,0 +1,15 @@
+function onClick(value){
+    console.log(value)
+    return value+1
+}
+const hello="gygbu"
+console.log(hello)
+const x=1
+const y=onClick(x)
+const o=onClick(y)
+const p=onClick(o)
+console.log(p)
+
+function f(x){
+    return 2*x+3
+}
